3ae2128086da4358740520794d5cebd2156a6aa7
Christoph Bornemann <bornemch@gmail.com>
2023-03-06 14:16:46 +0100

    First steps

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-let toDoFormInput = document.getElementById('')
+const toDoFormInput = document.getElementById('to-do-form')
+const add = document.getElementById('new-to-do');
+const toDoList = document.getElementById('to-do-items');
+let newToDo = '';
+
+addEventListener('change', (element) => {
+    newToDo = element.target.value;
+})
+
+const addNewToDo = (element) => {
+        element.preventDefault();
+
+        let listItem = document.createElement('li');
+        listItem.innerText = newToDo;
+        toDoList.appendChild(listItem);
+
+        add.value = '';
+}
+
+toDoFormInput.addEventListener('submit', addNewToDo);
